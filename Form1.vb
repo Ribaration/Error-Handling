d49fa6acb7bafc6826fcdd822d55c12dc64ef85a
Dell-User <ribalerato97@gmail.com>
2024-02-23 18:42:53 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+            Try
+
+                Dim userInput As Integer = Integer.Parse(TextBox1.Text)
+
+                Dim result As Integer = userInput * 3
+
+                Label1.Text = "Result: " & result.ToString()
+            Catch ex As FormatException
+
+                Label1.Text = "Error: Please enter a valid number."
+            End Try
+        End Sub
+
+
+End Class
